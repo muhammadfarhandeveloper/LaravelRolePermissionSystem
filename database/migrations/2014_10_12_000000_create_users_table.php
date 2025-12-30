a2e0c22afddacd9d8ffd95fd5ce21265adbde532
Muhammad Farhan <mohammadfarhan44500@gmail.com>
2025-12-31 00:28:48 +0500

    migration of users,category,posts complete

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,10 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('image');
+            $table->enum('role',['admin','manager','user'])->default('user');
             $table->rememberToken();
+            $table->enum('status',['active','inactive'])->default('active');
             $table->timestamps();
         });
     }
