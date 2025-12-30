b32e24b26a9233cdc48e54ef793ae98f5901e7a6
Muhammad Farhan <mohammadfarhan44500@gmail.com>
2025-12-31 00:48:05 +0500

    Make Models with fillable fields and relations

---
+++ app/Models/Post.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Post extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $fillable = ['title','slug','content','image','status','category_id','user_id'];
+
+
+    // Relations
+
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,9 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'image',
+        'role',
+        'status',
     ];
 
     /**
@@ -42,4 +45,11 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    // Relations
+
+    public function posts(){
+        return $this->hasMany(Post::class);
+    }
+
 }
