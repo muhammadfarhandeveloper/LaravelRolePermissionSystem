ca91f07c9dc3e204e21bd8c2f3f24eaa72194a40
Muhammad Farhan <mohammadfarhan44500@gmail.com>
2025-12-31 01:36:16 +0500

    image nullable in users migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('image');
+            $table->string('image')->nullable();
             $table->enum('role',['admin','manager','user'])->default('user');
             $table->rememberToken();
             $table->enum('status',['active','inactive'])->default('active');
